9f5958d99c056ac23bf26fdaa661bcf9c64abb9f
mmende <martin.mende@aristech.de>
2020-03-23 18:16:07 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# What does this library do?
+
+This library includes the geocoding and carrier mapping functionalities that are available in some of googles libphonenumber libraries but not in libphonenumber-js (a port of libphonenumber).
+To reduce the amount of data that needs to be loaded to geocode / carrier map a phonenumber for each mapping only the relevant number prefixes are loaded from a binary json file (BSON).
+When the prefix could not be found in the provided locale the library tries to fallback to `en` as locale.
+The library supports Node.js only at the moment.
+
+# Installation
+
+```sh
+npm install libphonenumber-js libphonenumber-geo-carrier
+```
+
+or
+
+```sh
+yarn add libphonenumber-js libphonenumber-geo-carrier
+```
+
+# Usage
+
+```js
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+import { geocoder, carrier } from 'libphonenumber-geo-carrier'
+
+const main = async () => {
+  const fixedLineNumber = parsePhoneNumberFromString('+41431234567')
+  const locationEN = await geocoder(fixedLineNumber) // Zurich
+  const locationDE = await geocoder(fixedLineNumber, 'de') // Zürich
+  const locationIT = await geocoder(fixedLineNumber, 'it') // Zurigo
+
+  const mobileNumber = parsePhoneNumberFromString('01701234567', 'DE')
+  const originalCarrier = await carrier(fixedLineNumber) // T-Mobile
+  // const originalCarrier = await carrier(fixedLineNumber) //
+}
+main()
+```
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import { PhoneNumber } from 'libphonenumber-js'
+import { GeocoderLocale, CarrierLocale } from './locales'
+import fs from 'fs'
+import { promisify } from 'util'
+import BSON from 'bson'
+import path from 'path'
+
+const access = promisify(fs.access)
+const readFile = promisify(fs.readFile)
+
+const getPrefix = (phonenumber: PhoneNumber) =>
+  phonenumber.formatInternational().replace(/^\+[0-9]+ ([0-9]+) .*/, '$1')
+
+const getCode = async (dataPath: string, prefix: string) => {
+  try {
+    await access(dataPath)
+    const bData = await readFile(dataPath)
+    const data = BSON.deserialize(bData)
+    const description = data[prefix]
+    return description as string
+  } catch (err) {}
+  return null
+}
+
+export const geocoder = async (
+  phonenumber: PhoneNumber | undefined,
+  locale: GeocoderLocale = 'en'
+) => {
+  if (!phonenumber) {
+    return null
+  }
+  const { countryCallingCode } = phonenumber
+  const prefix = getPrefix(phonenumber)
+  let dataPath = path.join(
+    __dirname,
+    '../resources/geocodes/',
+    locale,
+    `${countryCallingCode}.bson`
+  )
+  const code = await getCode(dataPath, prefix)
+  if (code) {
+    return code
+  }
+  if (locale !== 'en') {
+    // Try fallback to english
+    dataPath = path.join(
+      __dirname,
+      '../resources/geocodes/',
+      'en',
+      `${countryCallingCode}.bson`
+    )
+    return await getCode(dataPath, prefix)
+  }
+  return null
+}
+
+export const carrier = async (
+  phonenumber: PhoneNumber | undefined,
+  locale: CarrierLocale = 'en'
+) => {
+  if (!phonenumber) {
+    return null
+  }
+  const { countryCallingCode } = phonenumber
+  const prefix = getPrefix(phonenumber)
+  let dataPath = path.join(
+    __dirname,
+    '../resources/carrier/',
+    locale,
+    `${countryCallingCode}.bson`
+  )
+  const code = await getCode(dataPath, prefix)
+  if (code) {
+    return code
+  }
+  if (locale !== 'en') {
+    // Try fallback to english
+    dataPath = path.join(
+      __dirname,
+      '../resources/carrier/',
+      'en',
+      `${countryCallingCode}.bson`
+    )
+    return await getCode(dataPath, prefix)
+  }
+  return null
+}
