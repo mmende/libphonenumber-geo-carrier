457273d36af160ae35e3452ccdcebaadc9f04491
Martin Mende <martin.mende@aristech.de>
2021-06-02 08:15:03 +0200

    :bulb: Added jsdocs

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,23 @@ import path from 'path'
 const access = promisify(fs.access)
 const readFile = promisify(fs.readFile)
 
+/**
+ * Extracts the numbers national prefix which is used as key in the metadata bson files
+ *
+ * @param phonenumber The phonenumber object
+ * @returns The national prefix of the phonenumber (e.g. "+49 6221 123456" => "6221")
+ */
 const getPrefix = (phonenumber: PhoneNumber) =>
   phonenumber.formatInternational().replace(/^\+[0-9]+ ([0-9]+) .*/, '$1')
 
+/**
+ * Maps the dataPath and prefix to geocode, carrier or timezones of null if this info could not be extracted
+ *
+ * **Note:** Timezones are returned as single string joined with `&`
+ *
+ * @param dataPath Path of the metadata bson file to use
+ * @param prefix National prefix of the given number
+ */
 const getCode = async (dataPath: string, prefix: string) => {
   try {
     await access(dataPath)
@@ -24,6 +38,12 @@ const getCode = async (dataPath: string, prefix: string) => {
   return null
 }
 
+/**
+ * Provides geographical information related to the phone number
+ *
+ * @param phonenumber The phone number
+ * @param locale The preferred locale to use (falls back to `en` if there are no localized carrier infos for the given locale)
+ */
 export const geocoder = async (
   phonenumber: PhoneNumber | undefined,
   locale: GeocoderLocale = 'en'
@@ -56,6 +76,16 @@ export const geocoder = async (
   return null
 }
 
+/**
+ * Maps the phone number to the original carrier
+ *
+ * **Note:** This method cannot provide data about the current carrier of the phone number,
+ * only the original carrier who is assigned to the corresponding range.
+ * @see https://github.com/google/libphonenumber#mapping-phone-numbers-to-original-carriers
+ *
+ * @param phonenumber The phone number
+ * @param locale The preferred locale to use (falls back to `en` if there are no localized carrier infos for the given locale)
+ */
 export const carrier = async (
   phonenumber: PhoneNumber | undefined,
   locale: CarrierLocale = 'en'
@@ -88,6 +118,10 @@ export const carrier = async (
   return null
 }
 
+/**
+ * Provides all timezones related to the phone number
+ * @param phonenumber The phone number
+ */
 export const timezones = async (phonenumber: PhoneNumber | undefined) => {
   if (!phonenumber) {
     return null
